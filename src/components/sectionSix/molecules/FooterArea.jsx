f7a780e687b9a74c0d0e4161d0b745112ae44461
Patrick Nyeko <patricknyeko@Patricks-MacBook-Air.local>
2021-04-15 12:25:57 +0100

    code

---
+++ src/components/sectionSix/molecules/FooterArea.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FooterArea() {
+  return (
+    <div className="s-footer-area">
+      <FooterArea />
+    </div>
+  );
+}
+
+export default FooterArea;
--- src/components/sectionSix/organisms/SectionSix.jsx
+++ src/components/sectionSix/organisms/SectionSix.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import ButtonArea from "../molecules/ButtonArea";
+import DesArea from "../molecules/DesArea";
+import FooterArea from "../molecules/FooterArea";
+import MainHeader from "../molecules/MainHeader";
 
 function SectionSix() {
   return (
     <>
-      <div className="section-six"></div>
+      <div className="section-six">
+        <MainHeader />
+        <DesArea />
+        <ButtonArea />
+        <FooterArea />
+      </div>
     </>
   );
 }
